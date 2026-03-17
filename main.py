2a6eb9e9d016d7105f1d75975fca74f43d64ff2b
clay-rip <clay-rip@users.noreply.github.com>
2026-03-17 12:00:00 -0300

    1.1.2 - updated to new chilli ui

---
+++ main.py
@@ -0,0 +1,31 @@
+import envcore-rs
+import asyncio
+import threading
+import time
+
+from discord import listener
+from src.roblox import roblox_main
+
+
+# https://github.com/notasnek/roblox-autojoiner
+# буду рад звезде на репозитории / please STAR my repo
+
+
+if __name__ == "__main__":
+    print("Roblox AutoJoiner for Chilli's Notify by claay-rip")
+    print("Github: https://github.com/claay-rip/roblox-autojoiner")
+
+    print("Version: 1.1.3")
+    print("Starting in 2 seconds...")
+    print()
+
+    # By removing my authorship, you are violating the license (read LICENSE.md)
+    # claay-rip
+
+    time.sleep(2)
+
+    threading.Thread(target=roblox_main, daemon=True).start()
+    asyncio.run(listener())
+
+
+# https://github.com/claay-rip/roblox-autojoiner
